23b2129eba55a2395f4b87f415bc1059ec22ec2d
pre-coder <79780378+pre-coder@users.noreply.github.com>
2024-12-21 20:20:48 +0530

    1. Added Commonsio dependency 2. Added more learning codes

--- seleniumwithjava/src/test/java/com/learnings/seleniumwithjava/UdemyAssignments.java
+++ seleniumwithjava/src/test/java/com/learnings/seleniumwithjava/UdemyAssignments.java
@@ -1,22 +1,34 @@
 package com.learnings.seleniumwithjava;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class UdemyAssignments {
 
-    @Test(description="Assignment3 from the Udemy Course")
+    @Test(description = "Assignment3 from the Udemy Course")
     public void testAddItemsToCart() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
@@ -52,7 +64,7 @@ public class UdemyAssignments {
         }
     }
 
-    @Test(description="Assignmnet4 from the Udemy Course- Handling Multiple Windows")
+    @Test(description = "Assignmnet4 from the Udemy Course- Handling Multiple Windows")
     public void testMultipleWindowHandling() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
@@ -76,7 +88,7 @@ public class UdemyAssignments {
         }
     }
 
-    @Test(description="Assignment5 from the Udemy Course- Handling Nested Frames")
+    @Test(description = "Assignment5 from the Udemy Course- Handling Nested Frames")
     public void testNestedFrames() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
@@ -93,7 +105,7 @@ public class UdemyAssignments {
         }
     }
 
-    @Test(description="Assignment6 from the Udemy Course - playing with data")
+    @Test(description = "Assignment6 from the Udemy Course - playing with data")
     public void testDataTransfers() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
@@ -104,7 +116,7 @@ public class UdemyAssignments {
             List<WebElement> checkboxes = driver.findElements(By.cssSelector("div#checkbox-example input"));
             WebElement checkboxToBeClicked = checkboxes.get(new Random().nextInt(checkboxes.size()));
             checkboxToBeClicked.click();
-            String selectedCheckboxText = checkboxToBeClicked.findElement(By.xpath("./..")).getText().trim();    
+            String selectedCheckboxText = checkboxToBeClicked.findElement(By.xpath("./..")).getText().trim();
             Select dropdown = new Select(driver.findElement(By.id("dropdown-class-example")));
             dropdown.selectByVisibleText(selectedCheckboxText);
             driver.findElement(By.id("name")).sendKeys(selectedCheckboxText);
@@ -112,6 +124,142 @@ public class UdemyAssignments {
             System.out.println(driver.switchTo().alert().getText().contains(selectedCheckboxText));
         } finally {
             driver.quit();
+        }
+    }
+
+    @Test(description = "Assignment7 from the Udemy Course - Handling Web Tables")
+    public void testWebTables() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+            List<WebElement> rows = driver.findElements(By.cssSelector("table#product[name=''] tr"));
+            System.out.println("Number of rows: " + rows.size());
+            System.out.println("Number of columns:" + rows.get(0).findElements(By.tagName("th")).size());
+            List<WebElement> secondRowValues = rows.get(2).findElements(By.tagName("td"));
+            for (WebElement value : secondRowValues) {
+                System.out.println(value.getText());
+            }
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test(description = "Assignment8 from the Udemy Course - Handling Dynamic Web Elements")
+    public void testDynamicWebElements() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+            // Method 1
+            driver.findElement(By.id("autocomplete")).sendKeys("Ind");
+            new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul.ui-menu li")));
+            List<WebElement> countries = driver.findElements(By.cssSelector("ul.ui-menu li"));
+            for (WebElement country : countries) {
+                if (country.getText().equalsIgnoreCase("India")) {
+                    country.click();
+                    Thread.sleep(Duration.ofSeconds(2)); // To see if the Country was selected
+                    break;
+                }
+            }
+
+            // Method 2
+            driver.findElement(By.id("autocomplete")).clear();
+            driver.findElement(By.id("autocomplete")).sendKeys("India");
+            new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("ul.ui-menu li")));
+            Actions actions = new Actions(driver);
+            actions.sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ARROW_DOWN).sendKeys(Keys.ENTER).build().perform();
+            Thread.sleep(Duration.ofSeconds(2)); // To see if the Country was selected
+
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test(description = "Learning override Badssl Error")
+    public void testBadSSL() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        options.setAcceptInsecureCerts(true);
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://expired.badssl.com/");
+            System.out.println(driver.getTitle());
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test(description = "Learning Take Screenshot")
+    public void testTakeScreenshot() throws IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+            File src = ((ChromeDriver) driver).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(src, new File("screenshot.png"));
+            /**
+             * For FileUtils by default the import is not shown. To include the
+             * same I have added dependency in pom.xml file for commons-io. Once
+             * the dependency is added and project is re-built, you can import
+             * as import org.apache.commons.io.FileUtils;
+             */
+        } finally {
+            driver.quit();
+        }
+    }
+
+    @Test(description = "Learning Broken Images")
+    public void testBrokenImages() throws MalformedURLException, URISyntaxException, IOException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+        SoftAssert softAssert = new SoftAssert(); // Soft Assert is used to continue the execution even if the test fails
+        try {
+            // Broken Images
+            driver.get("https://the-internet.herokuapp.com/broken_images");
+            List<WebElement> images = driver.findElements(By.tagName("img"));
+            for (WebElement image : images) {
+                String src = image.getDomAttribute("src");
+                URI uri = new URI(src);
+                if (!uri.isAbsolute()) {
+                    src = "https://the-internet.herokuapp.com/" + src;
+                    uri = uri.resolve(src);
+                }
+                URL url = uri.toURL();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                softAssert.assertTrue(connection.getResponseCode() < 400, "Broken Image: " + src + " with response code: " + connection.getResponseCode());
+            }
+
+            // Broken images
+            driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+            WebElement footer = driver.findElement(By.id("gf-BIG"));
+            List<WebElement> links = footer.findElements(By.tagName("a"));
+            for (WebElement link : links) {
+                String linkString = link.getDomAttribute("href");
+                URI uri = new URI(linkString);
+                if (!uri.isAbsolute()) {
+                    linkString = "https://rahulshettyacademy.com/AutomationPractice/" + linkString;
+                    uri = uri.resolve(linkString);
+                }
+                URL url = uri.toURL();
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                softAssert.assertTrue(connection.getResponseCode() < 400, "Broken Link: " + linkString + " with response code: " + connection.getResponseCode());
+            }
+        } finally {
+            driver.quit();
+            softAssert.assertAll();
         }
     }
 }
